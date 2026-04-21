d39ec0c9d8b46ba961b642a44c603505aed1470b
nemar2510 <kaurnemarpreet2006@gmail.com>
2026-04-21 20:29:28 +0530

    connect backend via axios

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://todo-app-wyji.onrender.com/api"
 });
 
-// 🔥 ADD THIS (IMPORTANT)
+// 🔥 Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
