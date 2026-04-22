f20d6f4978a280167f1d21ced11553bbcfba0984
nemar2510 <kaurnemarpreet2006@gmail.com>
2026-04-22 13:16:41 +0530

    remove sw.js registration completely

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
+  import * as serviceWorker from './serviceWorker';
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
