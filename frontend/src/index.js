807a71c717c40b42634962e1ecdd18d57740f24e
nemar2510 <kaurnemarpreet2006@gmail.com>
2026-04-22 13:23:53 +0530

    remove service worker completely

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-if ('serviceWorker' in navigator) {
-  import * as serviceWorker from './serviceWorker';
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
