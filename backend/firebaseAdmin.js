b3483b0ede52626c15e6d27922434cf28c227d13
nemar2510 <kaurnemarpreet2006@gmail.com>
2026-04-24 00:25:29 +0530

    fix firebase env config

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
